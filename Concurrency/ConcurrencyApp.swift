e2f624aa419a4e5226d3904af8a837397634fc8b
efeaton86 <50210677+efeaton86@users.noreply.github.com>
2022-03-08 16:23:19 +0000

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Eric Eaton on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
